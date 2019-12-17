6c134d34fcace5e47cf93dabeb5f38f844af770e
mdevereux <michael.devereux@framestore.com>
2019-12-17 11:59:32 +0000

    fix-build

--- cmake/aws-c-cal-config.cmake
+++ cmake/aws-c-cal-config.cmake
@@ -4,9 +4,4 @@ if (NOT BYO_CRYPTO AND NOT WIN32 AND NOT APPLE)
     find_dependency(LibCryptoCAL)
 endif()
 
-if (BUILD_SHARED_LIBS)
-    include(${CMAKE_CURRENT_LIST_DIR}/shared/@CMAKE_PROJECT_NAME@-targets.cmake)
-else()
-    include(${CMAKE_CURRENT_LIST_DIR}/static/@CMAKE_PROJECT_NAME@-targets.cmake)
-endif()
-
+include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
